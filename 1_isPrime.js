1f502c1f61b940bc193b8ecd5a2997d159964c0b
devrathrana <devrath.rana@bold.com>
2024-12-25 15:18:51 +0530

    js

--- 1_isPrime.js
+++ 1_isPrime.js
@@ -1,18 +1,17 @@
 const isPrime = (inputNum) => {
-    let factorCount = 0;
-    for (let i = 1; i <= Math.sqrt(inputNum); i++) {
-      if (inputNum % i === 0) {
-        const b = inputNum / i;
-        if (b === i) {
-          factorCount += 1;
-        } else {
-          factorCount += 2;
-        }
+  let factorCount = 0;
+  for (let i = 1; i <= Math.sqrt(inputNum); i++) {
+    if (inputNum % i === 0) {
+      if (i * i === inputNum) {
+        factorCount += 1;
+      } else {
+        factorCount += 2;
       }
     }
-    return factorCount<=2;
-  };
-  
-  const isPrimeBool = isPrime(8);
-  
-  console.log('isPrimeBool>>>', isPrimeBool);
+  }
+  return factorCount <= 2;
+};
+
+const isPrimeBool = isPrime(8);
+
+console.log("isPrimeBool>>>", isPrimeBool);
