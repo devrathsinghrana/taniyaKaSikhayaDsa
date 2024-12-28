6adc1619bd86765539f017c5e6fa14e20e7d4bab
devrathrana <devrath.rana@bold.com>
2024-12-28 23:42:04 +0530

    js

--- 4_rotate_array_k_times_inright.js
+++ 4_rotate_array_k_times_inright.js
@@ -1,55 +1,73 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 234, 367, 8, 89];
 const n = arr.length;
-const numOfRotations = 13;
+const numOfRotations = 2;
 const k = numOfRotations % n;
 const rotatedArray = [];
-
+console.log("arr>>>", arr);
+console.log("numOfRotations>>>", numOfRotations);
+console.log("k>>>", k);
 // Outplace method
-for (let i = n - k; i < n; i++) {
-  rotatedArray.push(arr[i]);
+function outPlaceMethod() {
+  if (k === 0) {
+    console.log("rotatedArray by ouplace>>>", arr);
+    return;
+  }
+  for (let i = n - k; i < n; i++) {
+    rotatedArray.push(arr[i]);
+  }
+
+  for (let i = 0; i < n - k; i++) {
+    rotatedArray.push(arr[i]);
+  }
+
+  console.log("rotatedArray by ouplace>>>", rotatedArray);
 }
 
-for (let i = 0; i < n - k; i++) {
-  rotatedArray.push(arr[i]);
-}
-
-console.log("rotatedArray>>>", rotatedArray);
+outPlaceMethod();
 
 // Inplace Method
-const reverseArray = (inputArray) => {
-  const n = inputArray.length;
+function inplaceMethod() {
+  if (k === 0) {
+    console.log("rotatedArray by inplace>>>", arr);
+    return;
+  }
+  const reverseArray = (inputArray) => {
+    const n = inputArray.length;
+    let i = 0;
+    let j = n - 1;
+    while (i < n / 2 && j >= n / 2) {
+      const backupValueToBeSwapped = inputArray[i];
+      inputArray[i] = inputArray[j];
+      inputArray[j] = backupValueToBeSwapped;
+      i++;
+      j--;
+    }
+    //no need to return array as object is passed by reference
+  };
+
+  reverseArray(arr);
+
   let i = 0;
-  let j = n - 1;
-  while (i < n / 2 && j >= n / 2) {
-    const backupValueToBeSwapped = inputArray[i];
-    inputArray[i] = inputArray[j];
-    inputArray[j] = backupValueToBeSwapped;
+  let j = k - 1;
+  while (i < k / 2 && j >= k / 2) {
+    const backupValueToBeSwapped = arr[i];
+    arr[i] = arr[j];
+    arr[j] = backupValueToBeSwapped;
     i++;
     j--;
   }
-  //no need to return array as object is passed by reference
-};
 
-reverseArray(arr);
+  let y = k;
+  let z = n - 1;
+  while (y < n / 2 + 1 && z >= n / 2 + 1) {
+    const backupValueToBeSwapped = arr[y];
+    arr[y] = arr[z];
+    arr[z] = backupValueToBeSwapped;
+    y++;
+    z--;
+  }
 
-let i = 0;
-let j = k - 1;
-while (i < k / 2 && j >= k / 2) {
-  const backupValueToBeSwapped = arr[i];
-  arr[i] = arr[j];
-  arr[j] = backupValueToBeSwapped;
-  i++;
-  j--;
+  console.log("rotatedArray by inplace>>>", arr);
 }
 
-let y = k;
-let z = n - 1;
-while (y < n / 2 + 1 && z >= n / 2 + 1) {
-  const backupValueToBeSwapped = arr[y];
-  arr[y] = arr[z];
-  arr[z] = backupValueToBeSwapped;
-  y++;
-  z--;
-}
-
-console.log("rotatedArray by inplace>>>", arr);
+inplaceMethod();
