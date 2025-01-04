df1103e1f2361e2c1f9a82c87bd14960d90457b8
devrathrana <devrath.rana@bold.com>
2025-01-04 15:39:59 +0530

    js

--- 6_singleNumberArray.js
+++ 6_singleNumberArray.js
@@ -9,3 +9,10 @@ const singleNumberArray = (arrIn) => {
 };
 
 console.log(singleNumberArray(myArray));
+
+// Bitwise XOR returns 0 for same numbers - so it negates all the duplicates and give the only unique number in array
+
+const getUniqueSingleNumberInAnArray = (arrIn) =>
+  arrIn.reduce((a, b) => a ^ b, 0);
+
+console.log(getUniqueSingleNumberInAnArray(myArray));
